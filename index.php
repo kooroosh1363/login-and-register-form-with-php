2a0e50ccda3db6f11f6a035cdb7898d45cd164a7
kooroosh1363 <peymanraad1363@gmail.com>
2023-06-11 22:41:32 +0400

    change html to php and create config file

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+include("./partials/header.php")
+
+?>
+
+
+
+<div class="container_form">
+    <div class="overlay">
+        <!-- nothing content -->
+    </div>
+    <div class="divTitle">
+        <h1 class="title">LOGIN</h1>
+        <span class="subTitle">WELCOME BACK</span>
+    </div>
+
+    <!-- section form -->
+    <form action="" method="">
+        <div class="row grid">
+            <!-- username -->
+            <div class="row">
+                <label for="username">User Name</label>
+                <input type="text" id="username" name="username" placeholder="Enter User Name" required>
+            </div>
+            <!-- password -->
+            <div class="row">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter Your Password" required>
+            </div>
+            <!-- submit -->
+            <div class="row">
+                <input type="submit" id="sub_btn" name="submit" value="LOGIN">
+
+                <span class="linkRegister">Don't Have An Account?<a href="./register.html">Register</a></span>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+<?php
+include("./partials/footer.php")
+
+?>
