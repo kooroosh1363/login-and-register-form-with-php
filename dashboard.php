0fff001f6866cb156bb58912ddc8662e1a8547e5
kooroosh1363 <peymanraad1363@gmail.com>
2023-06-15 22:24:34 +0400

    change dashboard codes

--- dashboard.php
+++ dashboard.php
@@ -1,24 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./main.css">
-    <title>ACCOUNT IN PHP</title>
-</head>
-<body>
-    
-    <div class="dashboard">
-        <span>
+<?php
 
-        </span>
-        <h1>DASHBOARD</h1>
-        <div class="btnLogOut">
-            <a href="logout.html">Log out</a>
-        </div>
+include('./partials/header.php');
+
+?>
+
+<div class="dashboard">
+    <span>
+
+    </span>
+    <h1>DASHBOARD</h1>
+    <div class="btnLogOut">
+        <a href="logout.php">Log out</a>
     </div>
+</div>
 
 
-</body>
-</html>
+<?php
+
+include('./partials/footer.php');
+
+?>
